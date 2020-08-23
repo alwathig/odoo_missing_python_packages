5197c9826eaec97e267b307101bccd8bb27a1cb4
الواثق الحاج <alwathig777@gmail.com>
2020-08-23 18:02:05 +0400

    first commit

---
+++ missing.sh
@@ -0,0 +1,14 @@
+sudo pip3 install PyPDF2
+sudo pip3 install passlib
+sudo pip3 install babel
+sudo pip3 install werkzeug
+sudo pip3 install lxml
+sudo pip3 install decorator
+sudo pip3 install polib
+sudo pip3 install image
+sudo apt-get install python3-psycopg2
+sudo pip3 install dateutil
+sudo apt-get install python3-psutil
+sudo apt-get install python3-werkzeug
+sudo pip3 install reportlab
+pip3 install libsass==0.12.3
